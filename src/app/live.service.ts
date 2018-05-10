162c33b6b7853e50e0207e1e394eaab220dd884b
Guill76 <gllmcanu388@gmail.com>
2018-05-10 21:12:08 +0200

    ok

--- src/app/live.service.ts
+++ src/app/live.service.ts
@@ -5,33 +5,33 @@ import { ConfigService } from './config.service';
 
 @Injectable()
 export class LiveService {
-  liveResults :any;
-  liveComing : any;
+  liveResults: any;
+  liveComing: any;
   private tm;
-  show : boolean=false;
-  resObs :any;
-  goSlowly :boolean=true;
+  show = false;
+  resObs: any;
+  goSlowly = true;
 
-  constructor(private fdbServ: FootDbApiService, private notSrv:NotificationService, private config: ConfigService) { }
-  getLiveMatches(timeint){
-    this.tm=setInterval(()=>{
-      this.show=true;
+  constructor(private fdbServ: FootDbApiService, private notSrv: NotificationService, private config: ConfigService) { }
+  getLiveMatches(timeint) {
+    this.tm = setInterval(() => {
+      this.show = true;
       this.init();
-    },timeint);
+    }, timeint);
   }
 
-  init(){
+  init() {
     clearInterval(this.tm);
-    this.tm=null;
-    let now=new Date();
+    this.tm = null;
+    const now = new Date();
     let timeInterval;
-    this.resObs=this.fdbServ.getObsRequest('fixtures').subscribe(res=>{
-      this.show=true;
-      this.liveResults=res.fixtures.filter((val)=>{return val.status==='IN_PLAY'});
-      this.liveComing=res.fixtures.filter((val)=>{return (val.status==='TIMED'&& val.date > now.toISOString())});
-      //console.log("tableau next timeFrame",res.fixtures)
-      timeInterval=(this.liveResults && this.liveResults.length>0)?this.config.liveTimer:this.config.slowLiveTimer;
-      this.notSrv.notify('Le Timer du Live est actuellement à '+timeInterval+' ms','INFO',2000);
+    this.resObs = this.fdbServ.getObsRequest('fixtures').subscribe(res => {
+      this.show = true;
+      this.liveResults = res.fixtures.filter(val => val.status === 'IN_PLAY');
+      this.liveComing = res.fixtures.filter(val => (val.status === 'TIMED' && val.date > now.toISOString()));
+      // console.log("tableau next timeFrame",res.fixtures)
+      timeInterval = (this.liveResults && this.liveResults.length > 0) ? this.config.liveTimer : this.config.slowLiveTimer;
+      this.notSrv.notify('Le Timer du Live est actuellement à ' + timeInterval + ' ms', 'INFO', 2000);
       this.getLiveMatches(timeInterval);
     });
   }
--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+// tslint:disable-next-line:import-blacklist
 import * as Rx from 'rxjs/Rx';
 
 @Injectable()
@@ -10,8 +11,8 @@ export class WebsocketService {
   public connect(url): Rx.Subject<MessageEvent> {
     if (!this.subject) {
       this._subject = this.create(url);
-      console.log("Successfully connected: " + url);
-    } 
+      console.log('Successfully connected: ' + url);
+    }
     return this._subject;
   }
 
@@ -19,28 +20,28 @@ export class WebsocketService {
     return this._subject;
   }
   private create(url): Rx.Subject<MessageEvent> {
-    let ws = new WebSocket(url);
+    const ws = new WebSocket(url);
 
-    let observable = Rx.Observable.create(
-	(obs: Rx.Observer<MessageEvent>) => {
-    ws.onmessage = obs.next.bind(obs);
-    //ws.onopen = obs.next.bind(obs);
-		ws.onerror = obs.error.bind(obs);
-		ws.onclose = obs.complete.bind(obs);
-		return ws.close.bind(ws);
-	}).map(data=>{ 
-    console.log(typeof data.data,data.data);
-    console.log(data.data);
-    return (JSON.parse(data.data));
-  });
-let observer = {
-		next: (data: Object) => {
-			if (ws.readyState === WebSocket.OPEN) {
-				ws.send(JSON.stringify(data));
-			}
-		}
-	}
-	return Rx.Subject.create(observer, observable);
+    const observable = Rx.Observable.create(
+      (obs: Rx.Observer<MessageEvent>) => {
+        ws.onmessage = obs.next.bind(obs);
+        // ws.onopen = obs.next.bind(obs);
+        ws.onerror = obs.error.bind(obs);
+        ws.onclose = obs.complete.bind(obs);
+        return ws.close.bind(ws);
+    }).map(data => {
+      console.log(typeof data.data, data.data);
+      console.log(data.data);
+      return (JSON.parse(data.data));
+    });
+    const observer = {
+      next: (data: Object) => {
+        if (ws.readyState === WebSocket.OPEN) {
+          ws.send(JSON.stringify(data));
+        }
+      }
+    };
+    return Rx.Subject.create(observer, observable);
   }
 
 }
